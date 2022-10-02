08f4caa3a3d970dd282c0970cba3af1c3158208e
Ryohskay <learnerryo@gmail.com>
2022-10-02 20:59:42 +0100

    initial commit

---
+++ invandringar_svenska_OECD.py
@@ -0,0 +1,14 @@
+immigrants = [
+        50664, 50912, 50583, 49967, 48695, 52212, 80615, 82252, 78444,
+        86894, 79851, 87599, 99509, 108885, 118002, 121054, 154935,
+        132936, 123730, 102038
+        ]
+
+total = 0  # number of immigrants in total
+
+for i in immigrants:
+    total += i
+
+average = total/20
+print(total)
+print(average)
